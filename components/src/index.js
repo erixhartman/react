9b29a10deefe080b13efb16391182471a90b0b07
Eric Hartman <eric.hartman@loblaw.ca>
2019-08-12 15:16:00 -0400

    built initial component for card

--- components/src/index.js
+++ components/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
