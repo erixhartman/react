781bc6d57ce7747c3c35024ffe69620578fcca7d
Eric Hartman <eric.hartman@loblaw.ca>
2019-08-14 16:02:22 -0400

    async await and dont tell on me im committing a secret

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,17 @@
+import axios from 'axios';
 import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },    
+            headers: {
+                Authorization: 'Client-ID 4446ee75a5a0df8b2a078ee6f1739ba9ad329fddd180d010562d8a8decdbcc35'
+            }
+        });
+
+        console.log(response.data.results)
     }
  
     render() {
