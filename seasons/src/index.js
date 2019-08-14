d6cbeba8d5d096f8475f4c673b109ed0f4b64c53
Eric Hartman <eric.hartman@loblaw.ca>
2019-08-14 11:50:55 -0400

    remove conditional in render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,8 +7,7 @@ class App extends React.Component {
     
     state = { lat: null, errorMessage: '' }
     
-    // React says we HAVE TO define render!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage} </div>
         }
@@ -17,7 +16,15 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <Spinner />
+        return <Spinner message="Please accept location request"/>
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )        
     }
 
     componentDidMount() {
