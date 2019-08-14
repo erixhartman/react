b717488a89f97bff267c006ff2c845281e3082a9
Eric Hartman <eric.hartman@loblaw.ca>
2019-08-14 10:29:26 -0400

    state updated with latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,16 +9,18 @@ class App extends React.Component {
         super(props);
 
         this.state = { lat: null };
+        
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => console.log(err),
+        );
     }
     
     // React says we HAVE TO define render!
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err),
-        );
-
-        return <div>Latitude: </div>
+        return <div>Latitude: {this.state.lat} </div>
     }
 }
 
