545903ff0d063ae5b80fa7ae53d2ed88c201920f
Eric Hartman <eric.hartman@loblaw.ca>
2019-08-14 10:09:53 -0400

    initialized state in constuctor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    // Not required but also special
+    // First function called when this class is used.
+    // Good for initializing state
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+    
+    // React says we HAVE TO define render!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
