a3006397ad6903d3a26ccad28a0da9634a5f8b95
Eric Hartman <eric.hartman@loblaw.ca>
2019-10-02 14:39:51 -0400

    Crud done

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -14,12 +14,14 @@ const App = () => {
         <div className="ui container">
             <Router history={history}>
                 <div>
-                    <Header/>                    
-                    <Route path="/" exact component={StreamList}/>
-                    <Route path="/streams/new" component={StreamCreate}/>
-                    <Route path="/streams/edit/:id" component={StreamEdit}/>
-                    <Route path="/streams/delete/:id" component={StreamDelete}/>
-                    <Route path="/streams/show" component={StreamShow}/>
+                    <Header/>
+                    <Switch>
+                        <Route path="/" exact component={StreamList}/>
+                        <Route path="/streams/new" component={StreamCreate}/>
+                        <Route path="/streams/edit/:id" component={StreamEdit}/>
+                        <Route path="/streams/delete/:id" component={StreamDelete}/>
+                        <Route path="/streams/:id" component={StreamShow}/>
+                    </Switch>                    
                 </div>
             </Router>
         </div>
